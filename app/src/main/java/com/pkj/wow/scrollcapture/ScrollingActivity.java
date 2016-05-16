0a1ae1ca5d5f8f5dccc9a0079fdc1386a5d3f14d
pkjvit <pkjvit@gmail.com>
2016-05-16 08:14:05 +0530

    First commit
    First commit

---
+++ app/src/main/java/com/pkj/wow/scrollcapture/ScrollingActivity.java
@@ -0,0 +1,177 @@
+package com.pkj.wow.scrollcapture;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.widget.NestedScrollView;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class ScrollingActivity extends AppCompatActivity {
+
+    private ScrollView scroll_layout;
+    private LinearLayout main_layout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scrolling);
+        verifyStoragePermissions(this);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        scroll_layout = (ScrollView) findViewById(R.id.scroll_layout);
+        main_layout = (LinearLayout) findViewById(R.id.main_layout);
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                takeScreenShot();
+            }
+        });
+    }
+
+
+    // Storage Permissions
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
+    /**
+     * Checks if the app has permission to write to device storage
+     *
+     * If the app does not has permission then the user will be prompted to grant permissions
+     *
+     * @param activity
+     */
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+    }
+
+    private void takeScreenShot()
+    {
+        int totalHeight = scroll_layout.getChildAt(0).getHeight();
+        int totalWidth = scroll_layout.getChildAt(0).getWidth();
+
+        Bitmap b = getBitmapFromView(main_layout,totalHeight,totalWidth);
+
+        String filePath = Environment.getExternalStorageDirectory()
+                + File.separator + "Pictures/screenshot.png";
+        File imagePath = new File(filePath);
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(imagePath);
+            b.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+            fos.flush();
+            fos.close();
+            MediaStore.Images.Media.insertImage(getContentResolver(), b, "Screen", "screen");
+            sendMail(filePath);
+        }catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }   catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+
+    public static Bitmap getBitmapFromView(View view, int totalHeight, int totalWidth) {
+
+        int MAX_HEIGHT = totalHeight;
+        int height = Math.min(MAX_HEIGHT, totalHeight);
+        float percent = height / (float)totalHeight;
+
+        Bitmap canvasBitmap = Bitmap.createBitmap((int)(totalWidth*percent),(int)(totalHeight*percent), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(canvasBitmap);
+
+        Drawable bgDrawable = view.getBackground();
+        if (bgDrawable != null)
+            bgDrawable.draw(canvas);
+        else
+            canvas.drawColor(Color.WHITE);
+
+        canvas.save();
+        canvas.scale(percent, percent);
+        view.draw(canvas);
+        canvas.restore();
+
+        return canvasBitmap;
+    }
+
+
+    public void sendMail(String path) {
+        Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
+        emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL,
+                new String[] { "pkjvit@gmail.com" });
+        emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT,
+                "Truiton Test Mail");
+        emailIntent.putExtra(android.content.Intent.EXTRA_TEXT,
+                "This is an autogenerated mail from Truiton's InAppMail app");
+        emailIntent.setType("image/png");
+        Uri myUri = Uri.parse("file://" + path);
+        emailIntent.putExtra(Intent.EXTRA_STREAM, myUri);
+        startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_scrolling, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
